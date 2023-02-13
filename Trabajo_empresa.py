98427c9bd39bb1c378fc48e0ad60d9242ab66dda
Jose <al_jllaveroi@iessierradelasvillas.org>
2023-02-13 11:08:18 +0100

    cambios

--- Trabajo_empresa.py
+++ Trabajo_empresa.py
@@ -1,6 +1,10 @@
 import flet as ft
 
 def main(page: ft.Page):
-    vertical_alignment=ft.MainAxisAlignment.CENTER
+    vertical_alignment= ft.MainAxisAlignment.CENTER
+    img = ft.Image(src=f"/Imagenes/logo2.png", width=100, height=100)
+    
+    
 
-ft.app(target=main)  
+    page.add(img)
+ft.app(target=main, assets_dir="Imagenes")  
