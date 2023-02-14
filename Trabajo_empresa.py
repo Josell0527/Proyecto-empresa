a5610136d38f21bae4ee80aa3abfa4d555dff22c
Jose <al_jllaveroi@iessierradelasvillas.org>
2023-02-14 14:24:42 +0100

    Componentes

--- Trabajo_empresa.py
+++ Trabajo_empresa.py
@@ -1,10 +1,17 @@
 import flet as ft
 
 def main(page: ft.Page):
-    vertical_alignment= ft.MainAxisAlignment.CENTER
+    page.bgcolor= "#dcdcdc"
+
+    container_1.alignment = alignment.center
     img = ft.Image(src=f"/Imagenes/logo2.png", width=100, height=100)
     
+    usuario = ft.TextField(label="Usuario", width=300)
+    contraseña = ft.TextField(label="Contraseña", width=300)
     
+    botonguardar=ft.FilledButton(text="Iniciar sesión")
 
-    page.add(img)
+    
+    page.add(img, usuario, contraseña, botonguardar)
+    
 ft.app(target=main, assets_dir="Imagenes")  
