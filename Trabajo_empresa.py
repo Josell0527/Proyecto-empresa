63719db4f6e63109faa8b65e7257492572c92205
Inés <al_ijimenezg@iessierradelasvillas.org>
2023-02-17 11:08:44 +0100

    Funcionalidad

--- Trabajo_empresa.py
+++ Trabajo_empresa.py
@@ -2,7 +2,7 @@ import flet as ft
 
 def main(page: ft.Page):
 
-
+    
 
     def leerFichero():
         #Devolver la lista con los ususarios y contraseñas
@@ -24,14 +24,31 @@ def main(page: ft.Page):
         vIntroducidos.append(contraseña.value)
         print (vIntroducidos)
         #Comprobar login
-      
-        if (vDatos[0]==vIntroducidos[0])and (vDatos[1]==vIntroducidos[1]):
-            print ("Entrar")
+
+        if intentos.value>1:
+            if (vDatos[0]==vIntroducidos[0])and (vDatos[1]==vIntroducidos[1]):
+                print ("Entrar")
+                dlg1 = ft.AlertDialog(title=ft.Text("Sesión iniciada"),on_dismiss=lambda e: print("Dialog dismissed!"))
+                page.dialog = dlg1
+                dlg1.open = True
+                page.update()
+            else:
+                dlg = ft.AlertDialog(title=ft.Text("Usuario y contraseña incorrectos"),on_dismiss=lambda e: print("Dialog dismissed!"))
+                page.dialog = dlg
+                dlg.open = True
+                usuario.value=""
+                contraseña.value=""
+                intentos.value=int(intentos.value)-1
+                print(intentos.value)
+                page.update()
         else:
-            dlg = ft.AlertDialog(title=ft.Text("Usuario y contraseña incorectos"),on_dismiss=lambda e: print("Dialog dismissed!"))
-            page.dialog = dlg
-            dlg.open = True
+            usuario.value=""
+            contraseña.value=""
+            dlg2 = ft.AlertDialog(title=ft.Text("Número de intentos alcalzado"),on_dismiss=lambda e: print("Dialog dismissed!"))
+            page.dialog = dlg2
+            dlg2.open = True
             page.update()
+            page.close()
 
 
     #page.bgcolor=
@@ -42,7 +59,8 @@ def main(page: ft.Page):
     img = ft.Image(src=f"/Imagenes/logo3.png", width=200, height=200)
     
     usuario = ft.TextField(label="Usuario", width=300)
-    contraseña = ft.TextField(label="Contraseña", width=300)
+    contraseña = ft.TextField(label="Contraseña", width=300, password=True, can_reveal_password=True)
+    intentos = ft.TextField(value=3)
     
     botonguardar=ft.FilledButton(text="Iniciar sesión", on_click=comprobardatos)
 
---
+++ pruebas.py
@@ -0,0 +1,66 @@
+import flet as ft
+
+def main(page: ft.Page):
+
+
+
+    def leerFichero():
+        #Devolver la lista con los ususarios y contraseñas
+        vDatos = []
+        f = open("usuario.txt", "r") 
+        for i in f:
+            vDatos.append(i.replace("\n",""))
+        f.close()
+
+        return vDatos
+    
+    def close_dlg(e):
+        dlg_modal.open = False
+        page.update()
+
+    def comprobardatos(e):
+        #Obtener datos del fichero de contraseñas
+        vDatos = leerFichero()
+        print (vDatos)
+        #Obtener datos introducidos por teclado
+        vIntroducidos=[]
+        vIntroducidos.append(usuario.value)
+        vIntroducidos.append(contraseña.value)
+        print (vIntroducidos)
+        #Comprobar login
+      
+        if (vDatos[0]==vIntroducidos[0])and (vDatos[1]==vIntroducidos[1]):
+            print ("Entrar")
+            dlg_modal = ft.AlertDialog(
+                modal=True,title=ft.Text("Contraseña y usuario icorrectos"),
+                actions=[ft.TextButton("Volver a intentar", on_click=close_dlg)],
+                actions_alignment=ft.MainAxisAlignment.END,
+                on_dismiss=lambda e: print("Modal dialog dismissed!"))
+            page.dialog=dlg_modal
+            dlg.open = True
+            page.update()
+
+        else:
+            dlg = ft.AlertDialog(title=ft.Text("Usuario y contraseña incorectos"),on_dismiss=lambda e: print("Dialog dismissed!"))
+            page.dialog = dlg
+            dlg.open = True
+            page.update()
+
+
+    #page.bgcolor=
+    page.vertical_alignment = ft.MainAxisAlignment.CENTER
+    page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
+    page.window_resizable = False
+    
+    img = ft.Image(src=f"/Imagenes/logo3.png", width=200, height=200)
+    
+    usuario = ft.TextField(label="Usuario", width=300)
+    contraseña = ft.TextField(label="Contraseña", width=300)
+    
+    botonguardar=ft.FilledButton(text="Iniciar sesión", on_click=comprobardatos)
+
+    
+    page.add(img, usuario, contraseña, botonguardar)
+    
+    
+ft.app(target=main, assets_dir="Imagenes")  
